4d1325352cdb189488a52ffc69313098ed830f2a
Ben Welterveden <bwelterv@zhdk.ch>
2024-11-27 13:43:32 +0100

    Update new

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -1,29 +1,61 @@
-let len = 10;
-let bg;
+let dots = [];
+let font;
+let fontSize = 200;
+let word = "*DOTTED*";
+
+function preload() {
+  font = loadFont('/Assets/Univers 73 Black Extended Oblique.otf');
+}
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(10, 200, 58);
+  createCanvas(windowWidth, windowHeight);
+  textFont(font);
+  textSize(fontSize);
+
+  // center text
+  let bounds = font.textBounds(word, 0, 0, fontSize);
+  let xOffset = (width - bounds.w) / 2 - bounds.x;
+  let yOffset = (height - bounds.h) / 2 - bounds.y;
+
+  let spacing = 8; // spacing between dots
+
+  // dots for text
+  let textPoints = font.textToPoints(word, xOffset, yOffset, fontSize, {
+    sampleFactor: 0.2, // Density of dots
+  });
+
+  for (let pt of textPoints) {
+    dots.push(new Dot(pt.x, pt.y));
+  }
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(20,250, 40);
-	
-	rect(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-	rect(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(89, 30, 33);
-	}
+function draw() {
+  background(41, 41, 255);
+  for (let dot of dots) {
+    dot.update();
+    dot.show();
+  }
 }
 
+class Dot {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 4; // default dot size
+    this.maxSize = 25; // max. size hovered
+    this.minSize = 2; // min. size far away
+  }
+
+  update() {
+    let d = dist(mouseX, mouseY, this.x, this.y);
+		
+    this.size = map(d, 0, width / 5, this.maxSize, this.minSize);
+    this.size = constrain(this.size, this.minSize, this.maxSize);
+  }
+
+  show() {
+    noStroke();
+    fill(255); // white dots
+    ellipse(this.x, this.y, this.size);
+  }
+}
--- projects/01MainAssignments/project2/script.js
+++ projects/01MainAssignments/project2/script.js
@@ -1,29 +1,70 @@
-let len = 10;
-let bg;
+let taskbarPNG; 
+let taskbars = []; 
+let taskbarHeight = 50; 
+let offset = 5; 
+let debugColor = "rgba(0, 0, 0, 0)"; 
+
+function preload() {
+  taskbarPNG = loadImage("/Assets/Taskbar_Design.png");
+}
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(200, 0, 58);
+  createCanvas(windowWidth, windowHeight);
+  taskbarPNG.resize(taskbarPNG.width / 10, taskbarPNG.height / 10); //main taskbar size
+  addTaskbar(); 
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(0,130, 140);
-	
-	ellipse(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-  	ellipse(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(189, 3, 33);
-	}
+function draw() {
+  background(0);
+
+  for (let i = 0; i < taskbars.length; i++) {
+    drawTaskbar(taskbars[i].x, taskbars[i].y, i === taskbars.length - 1);
+  }
 }
 
+function drawTaskbar(x, y, isTop) {
+  image(taskbarPNG, x, y, taskbarPNG.width, taskbarPNG.height);
+
+  let buttonX = x + taskbarPNG.width - 300; // button position
+  let buttonY = y + 185; 
+  let buttonWidth = 210; 
+  let buttonHeight = 70; 
+
+  fill(debugColor); 
+  noStroke(); 
+  rect(buttonX, buttonY, buttonWidth, buttonHeight);
+}
+
+function addTaskbar() {
+  // taskbars stay in canvas
+  let maxX = width - taskbarPNG.width;
+  let maxY = height - taskbarPNG.height;
+
+  let randomX = random(0, maxX > 0 ? maxX : 0);
+  let randomY = random(0, maxY > 0 ? maxY : 0);
+
+  taskbars.push({ x: randomX, y: randomY });
+}
+
+function mousePressed() {
+  for (let i = 0; i < taskbars.length; i++) {
+    let buttonX = taskbars[i].x + taskbarPNG.width - 300; 
+    let buttonY = taskbars[i].y + 185; 
+    let buttonWidth = 210; 
+    let buttonHeight = 70; 
+
+    if (
+      mouseX > buttonX &&
+      mouseX < buttonX + buttonWidth &&
+      mouseY > buttonY &&
+      mouseY < buttonY + buttonHeight
+    ) {
+      addTaskbar(); 
+      break; 
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
